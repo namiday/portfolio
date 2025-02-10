5e4d4b2b4f849fb2c46080ba5ee42e37eb4e392e
namiday <dovakh0123@gmail.com>
2025-02-10 23:12:25 +0100

    maj floatingNavbar

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -8,7 +8,6 @@ import {
 } from "framer-motion";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { useMediaQuery } from "react-responsive";
 
 export const FloatingNav = ({
   navItems,
@@ -24,13 +23,9 @@ export const FloatingNav = ({
   const { scrollYProgress } = useScroll();
   const [visible, setVisible] = useState(true);
 
-  // Media query to check if viewport width is less than 900px
-  const isSmallScreen = useMediaQuery({ query: "(max-width: 900px)" });
-
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     if (typeof current === "number") {
       const direction = current - scrollYProgress.getPrevious();
-
       if (scrollYProgress.get() < 0.05) {
         setVisible(true);
       } else {
@@ -46,7 +41,7 @@ export const FloatingNav = ({
         animate={{ y: visible ? 0 : -100, opacity: visible ? 1 : 0 }}
         transition={{ duration: 0.2 }}
         className={cn(
-          "flex fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/10 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
+          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/10 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
           className
         )}
         style={{
@@ -54,19 +49,19 @@ export const FloatingNav = ({
           backgroundColor: "rgba(17, 25, 40, 0.75)",
           borderRadius: "12px",
           border: "1px solid rgba(255, 255, 255, 0.125)",
-          width: isSmallScreen ? "90%" : "auto",
         }}
       >
-        {navItems.map((navItem, idx) => (
+        {navItems.map((navItem: any, idx: number) => (
           <Link
             key={`link-${idx}`}
             href={navItem.link}
             className={cn(
-              "relative items-center flex space-x-1 text-neutral-600 dark:text-neutral-50 hover:text-neutral-500 dark:hover:text-neutral-300"
+              "relative items-center flex space-x-1 text-neutral-600 dark:text-neutral-50 dark:hover:text-neutral-300 hover:text-neutral-500",
+              "text-base md:text-sm lg:text-base" // Change the text size based on screen size
             )}
           >
-            {navItem.icon && <span className="block sm:hidden">{navItem.icon}</span>}
-            <span className="text-sm cursor-pointer">{navItem.name}</span>
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="cursor-pointer">{navItem.name}</span>
           </Link>
         ))}
       </motion.div>
